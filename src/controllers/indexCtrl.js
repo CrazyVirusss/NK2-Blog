7c474fb4eca529ee841fd8fc027a898b62915402
SlaneYang <904509221@qq.com>
2016-09-29 01:51:03 +0800

    creat project

---
+++ src/controllers/indexCtrl.js
@@ -0,0 +1,33 @@
+import mysql from 'mysql2'
+import config from '../config'
+
+export default async (ctx, next) => {
+  // const title = 'koa2 title'
+  let param = {
+    title:'koa2 title',
+    id:'',
+    name:''
+  }
+  const db = mysql.createConnection(config.db);
+  let id,name;
+
+  await db.connect(function (err) {
+    if (err) {
+      console.log("连接失败" + err);
+    } else {
+      console.log('连接成功！');
+    }
+  });
+  console.log(db + 'old')
+  console.log(await db.query('select * from test where id = 2', function (err, res) {
+    if (err) throw err;
+    if (res) {
+      console.log(param)
+      param.id = res[0].id;
+      param.name = res[0].name;
+      console.log(param)
+    }
+  }))
+  await ctx.render('index', {param});
+  await db.end();
+}
